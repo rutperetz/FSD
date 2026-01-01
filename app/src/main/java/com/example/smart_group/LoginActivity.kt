425759863f72a54bb6aa7d6f17144496ea192672
anatstefanski <anatstefanski16@gmail.com>
2026-01-01 23:44:05 +0200

    login(1)

--- app/src/main/java/com/example/smart_group/LoginActivity.kt
+++ app/src/main/java/com/example/smart_group/LoginActivity.kt
@@ -1,16 +1,12 @@
 package com.example.smart_group
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 
-class MainActivity : ComponentActivity() {
+class LoginActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        // Redirect immediately to LoginActivity
-        startActivity(Intent(this, LoginActivity::class.java))
-        finish()
+        setContentView(R.layout.activity_login)
     }
 }
