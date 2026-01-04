116da72a418971014b27ce2df62a02609c8c6f17
tehilaalon2003-cmyk <tehilaalon2003@gmail.com>
2026-01-04 20:27:04 +0200

    Updated courses layout preview

--- app/src/main/java/com/example/smart_group/CourseAdapter.kt
+++ app/src/main/java/com/example/smart_group/CourseAdapter.kt
@@ -5,6 +5,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.fsd.R
 
 class CourseAdapter(
     private val items: List<CourseUi>,
