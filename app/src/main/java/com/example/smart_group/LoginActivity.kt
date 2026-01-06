9d1c582bf8efe0084b3c63bb156bd1289f173008
anatstefanski <anatstefanski16@gmail.com>
2026-01-06 23:02:07 +0200

    Add forgot password UI flow (forgot, verify code, set new password)

--- app/src/main/java/com/example/smart_group/LoginActivity.kt
+++ app/src/main/java/com/example/smart_group/LoginActivity.kt
@@ -1,12 +1,22 @@
 package com.example.smart_group
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
+import androidx.appcompat.app.AppCompatActivity
+import com.example.fsd.R
+import android.content.Intent
+import android.widget.TextView
 
-class LoginActivity : ComponentActivity() {
+
+class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val forgotPassword = findViewById<TextView>(R.id.forgot_password_link_text)
+        forgotPassword.setOnClickListener {
+            startActivity(Intent(this, Forgot_PasswordActivity::class.java))
+        }
+
     }
 }
