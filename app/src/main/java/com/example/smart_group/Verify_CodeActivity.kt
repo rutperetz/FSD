09d10f8355f2c8911751ba3a29d1164b8d125d47
anatstefanski <anatstefanski16@gmail.com>
2026-01-06 23:18:49 +0200

    .

---
+++ app/src/main/java/com/example/smart_group/Verify_CodeActivity.kt
@@ -0,0 +1,30 @@
+package com.example.smart_group
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.fsd.R
+import com.google.android.material.button.MaterialButton
+
+class Verify_CodeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_verify_code)
+
+        val backArrow = findViewById<ImageView>(R.id.back_arrow)
+        val continueBtn = findViewById<MaterialButton>(R.id.continue_btn)
+
+        //Returns to previous screen
+        backArrow.setOnClickListener {
+            finish()
+        }
+
+        //check code and moves to the next screen
+        continueBtn.setOnClickListener {
+            startActivity(Intent(this, Set_New_PasswordActivity::class.java))
+        }
+    }
+}
