557b243675f6c9a9c3746bc54de9f33d8d754ef2
Rut Peretz <rutiperetz2141@gmail.com>
2026-01-06 20:33:22 +0200

    Register xml +navigation from Login to Register

---
+++ app/src/main/java/com/example/smart_group/RegisterActivity.kt
@@ -0,0 +1,16 @@
+package com.example.smart_group
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.fsd.R
+
+class RegisterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
