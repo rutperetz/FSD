b5bf57dfd5995a47981bad2e7b213c751127b4aa
anatstefanski <anatstefanski16@gmail.com>
2026-01-26 17:11:18 +0200

    logo

---
+++ app/src/main/java/com/example/smart_group/SplashActivity.kt
@@ -0,0 +1,22 @@
+package com.example.smart_group
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import com.example.smart_group.ui.login.LoginActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
