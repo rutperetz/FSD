39a43e35c79aa60bcc217327b169187d132778db
tehilaalon2003-cmyk <tehilaalon2003@gmail.com>
2026-03-09 14:05:31 +0200

    Add courses UI screen with XML layout and images

--- app/src/main/java/com/example/smart_group/MainActivity.kt
+++ app/src/main/java/com/example/smart_group/MainActivity.kt
@@ -3,16 +3,14 @@ package com.example.smart_group
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import com.example.smart_group.ui.login.LoginActivity
-
+import com.example.smart_group.ui.courses.CoursesActivity
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-//        // Redirect immediately to LoginActivity
-//        startActivity(Intent(this, LoginActivity::class.java))
-//        finish()
+        startActivity(Intent(this, CoursesActivity::class.java))
+        finish()
     }
 }
---
+++ app/src/main/java/com/example/smart_group/ui/courses/CourseCard.kt
@@ -0,0 +1,7 @@
+package com.example.smart_group.ui.courses
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun CourseCard() {
+}
