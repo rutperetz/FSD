940f9ca1882043d00eaac47d0a2c65c318664ee8
Rut Peretz <rutiperetz2141@gmail.com>
2026-03-09 22:25:21 -0400

    Questionnaire+Edit Questionnaire+Register

--- app/src/main/java/com/example/smart_group/ui/login/LoginActivity.kt
+++ app/src/main/java/com/example/smart_group/ui/login/LoginActivity.kt
@@ -8,9 +8,10 @@ import android.widget.Toast
 import com.example.smart_group.R
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import com.example.smart_group.ui.courses.CoursesActivity
 import com.example.smart_group.ui.login.LoginViewModel
 import com.example.smart_group.ui.register.RegisterActivity
-import com.example.smart_group.Forgot_PasswordActivity
+import com.example.smart_group.ui.forgotpassword.ForgotPasswordActivity
 import com.google.android.material.button.MaterialButton
 
 class LoginActivity : AppCompatActivity() {
@@ -42,7 +43,7 @@ class LoginActivity : AppCompatActivity() {
 
         //לחיצה על שכחתי סיסמא
         forgotPasswordText.setOnClickListener {
-            startActivity(Intent(this, Forgot_PasswordActivity::class.java))
+            startActivity(Intent(this, ForgotPasswordActivity::class.java))
         }
 
         //מאזין לתוצאות שבאו מהVIEWMODLE
@@ -64,9 +65,9 @@ class LoginActivity : AppCompatActivity() {
             if (go) {
                 Toast.makeText(this, "Login successful ✅", Toast.LENGTH_SHORT).show()
 
-                // כאן בעתיד תעברי למסך הבא (כשיהיה לך)
-                // startActivity(Intent(this, HomeActivity::class.java))
-                // finish()
+                //כאן בעתיד תעברי למסך הבא (כשיהיה לך)
+                startActivity(Intent(this, CoursesActivity::class.java))
+                finish()
 
                 viewModel.onNavigated()
             }
