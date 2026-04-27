0a44d592e8630c764553f9d438f45dac6e74b5b1
Rut Peretz <rutiperetz2141@gmail.com>
2026-04-27 21:04:28 +0300

    Update search page

---
+++ app/src/main/java/com/example/smart_group/ui/login/LoginActivity.kt
@@ -0,0 +1,75 @@
+package com.example.smart_group.ui.login
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import com.example.smart_group.R
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import com.example.smart_group.ui.register.RegisterActivity
+import com.example.smart_group.ui.forgotpassword.ForgotPasswordActivity
+import com.google.android.material.button.MaterialButton
+import com.example.smart_group.ui.home.HomeActivity
+
+class LoginActivity : AppCompatActivity() {
+
+    private val viewModel: LoginViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        val emailInput = findViewById<EditText>(R.id.email_input)
+        val passwordInput = findViewById<EditText>(R.id.password_input)
+        val loginBtn = findViewById<MaterialButton>(R.id.login_btn)
+        val registerText = findViewById<TextView>(R.id.register_link_text)
+        val forgotPasswordText = findViewById<TextView>(R.id.forgot_password_link_text)
+
+        loginBtn.setOnClickListener {
+            viewModel.login(
+                emailRaw = emailInput.text.toString(),
+                passwordRaw = passwordInput.text.toString()
+            )
+        }
+
+        registerText.setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
+
+        forgotPasswordText.setOnClickListener {
+            startActivity(Intent(this, ForgotPasswordActivity::class.java))
+        }
+
+        //Toast messages
+        viewModel.toastMessage.observe(this) { msg ->
+            if (msg != null) {
+                Toast.makeText(this, msg, Toast.LENGTH_LONG).show()
+                viewModel.onToastShown()
+            }
+        }
+
+        viewModel.isLoading.observe(this) { loading ->
+            loginBtn.isEnabled = !loading
+        }
+
+        viewModel.navigateToStudent.observe(this) { go ->
+            if (go) {
+                Toast.makeText(this, "Login successful", Toast.LENGTH_SHORT).show()
+                startActivity(Intent(this, HomeActivity::class.java))
+                finish()
+                viewModel.onStudentNavigated()
+            }
+        }
+
+        viewModel.navigateToAdmin.observe(this) { go ->
+            if (go) {
+                Toast.makeText(this, "Login successful", Toast.LENGTH_SHORT).show()
+                startActivity(Intent(this, HomeActivity::class.java))
+                finish()
+                viewModel.onAdminNavigated()
+            }
+        }
+    }
+}
